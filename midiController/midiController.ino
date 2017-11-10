5ff9d543bde21708de7d0f510f345d196261f754
Brady Boettcher <bboettcher2@wisc.edu>
2017-11-10 16:03:24 -0600

    rotary encoder works
    yay!

--- midiController/midiController.ino
+++ midiController/midiController.ino
@@ -1,4 +1,5 @@
-#include <Bounce.h>
+#include <Bounce.h> //used for buttons
+#include <Encoder.h> //used for rotary encoder
 
 //#define POT_PIN 3 //analog
 //#define TX_PIN 1 //digital
@@ -7,14 +8,21 @@
 #define BUTTON_2 1 //digital
 #define BUTTON_3 2 //digital
 #define BUTTON_4 3 //digital
-#define FADER 0 //analog
+#define FADER A0 //analog
+#define ENCODERPIN_1 4 //digital
+#define ENCODERPIN_2 5 //digital
 
 int faderVal = 0;
 int lastFaderVal = 0;
+long encoderVal = 0;
+long oldEncoderVal = 0;
+//buttons
 Bounce button1 = Bounce(BUTTON_1, 5);
 Bounce button2 = Bounce(BUTTON_2, 5);
 Bounce button3 = Bounce(BUTTON_3, 5);
 Bounce button4 = Bounce(BUTTON_4, 5);
+//jog wheel
+Encoder encoder(ENCODERPIN_1, ENCODERPIN_2);
 
 void setup() {
   // Set MIDI baud rate:
@@ -29,7 +37,8 @@ void loop() {
     button2.update();
     button3.update();
     button4.update();
-  faderVal = analogRead(A0) / 8; //divide by 8 to put in 0-127 range
+  encoderVal = encoder.read();
+  faderVal = analogRead(FADER) / 8; //divide by 8 to put in 0-127 range
   //Serial.println(analogRead(0));
   delay(10);
   if (button1.fallingEdge()) {
@@ -58,6 +67,11 @@ void loop() {
     Serial.print("Fader: ");
     Serial.println(faderVal);
     //delay(10);
+  }
+  if (encoderVal != oldEncoderVal) {
+    oldEncoderVal = encoderVal;
+    Serial.print("Encoder: ");
+    Serial.println(encoderVal);
   }
   delay(10); // to prevent slight fluctuations from sending through
   // MIDI Controllers should discard incoming MIDI messages.
