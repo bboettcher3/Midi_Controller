630c2e5070f723acae06ad529863c9a3a36d1d29
Brady Boettcher <bboettcher2@wisc.edu>
2017-11-11 01:10:18 -0600

    cleaned code, added correct CCs

--- midiController/midiController.ino
+++ midiController/midiController.ino
@@ -13,21 +13,20 @@
 #define ENCODERPIN_1 4 //digital
 #define ENCODERPIN_2 5 //digital
 
-int faderVal = 0;
-int lastFaderVal = 0;
-long encoderVal = 0;
-long oldEncoderVal = 0;
+int faderVal = 0; //current fader position
+int lastFaderVal = 0; //last received fader position
+long encoderVal = 0; //current encoder value
+long oldEncoderVal = 0; //last received encoder value
 //buttons
-Bounce button1 = Bounce(BUTTON_1, 5);
-Bounce button2 = Bounce(BUTTON_2, 5);
-Bounce button3 = Bounce(BUTTON_3, 5);
-Bounce button4 = Bounce(BUTTON_4, 5);
-Bounce enc_button = Bounce(ENCODER_BUTTON, 5);
-//jog wheel
-Encoder encoder(ENCODERPIN_1, ENCODERPIN_2);
+Bounce button1 = Bounce(BUTTON_1, 5); //CC1
+Bounce button2 = Bounce(BUTTON_2, 5); //CC2
+Bounce button3 = Bounce(BUTTON_3, 5); //CC3
+Bounce button4 = Bounce(BUTTON_4, 5); //CC4
+Bounce enc_button = Bounce(ENCODER_BUTTON, 5); //encoder button (CC5)
+Encoder encoder(ENCODERPIN_1, ENCODERPIN_2); //jog wheel (rotary encoder) (CC20)
 
 void setup() {
-  // Set MIDI baud rate:
+  //configure pins
   pinMode(BUTTON_1, INPUT_PULLUP);
   pinMode(BUTTON_2, INPUT_PULLUP);
   pinMode(BUTTON_3, INPUT_PULLUP);
@@ -36,48 +35,50 @@ void setup() {
 }
 
 void loop() {
-    button1.update();
-    button2.update();
-    button3.update();
-    button4.update();
-    enc_button.update();
+  //update buttons, fader and encoder
+  button1.update();
+  button2.update();
+  button3.update();
+  button4.update();
+  enc_button.update();
   encoderVal = encoder.read();
   faderVal = analogRead(FADER) / 8; //divide by 8 to put in 0-127 range
-  //Serial.println(analogRead(0));
-  delay(10);
+
+  //check for button presses
   if (button1.fallingEdge()) {
     //sendControlChange(control, value, channel)
-    usbMIDI.sendControlChange(16, 10, CHANNEL);
+    usbMIDI.sendControlChange(1, 10, CHANNEL);
     Serial.println("pressed 1");
   }
   if (button2.fallingEdge()) {
     //sendControlChange(control, value, channel)
-    usbMIDI.sendControlChange(17, 10, CHANNEL);
+    usbMIDI.sendControlChange(2, 10, CHANNEL);
     Serial.println("pressed 2");
   }
   if (button3.fallingEdge()) {
     //sendControlChange(control, value, channel)
-    usbMIDI.sendControlChange(18, 10, CHANNEL);
+    usbMIDI.sendControlChange(3, 10, CHANNEL);
     Serial.println("pressed 3");
   }
   if (button4.fallingEdge()) {
     //sendControlChange(control, value, channel)
-    usbMIDI.sendControlChange(19, 10, CHANNEL);
+    usbMIDI.sendControlChange(4, 10, CHANNEL);
     Serial.println("pressed 4");
   }
   if (enc_button.fallingEdge()) {
     //sendControlChange(control, value, channel)
-    //usbMIDI.sendControlChange(19, 10, CHANNEL);
+    usbMIDI.sendControlChange(5, 10, CHANNEL);
     Serial.println("pressed encoder");
   }
+  //check for change in fader or encoder
   if (faderVal != lastFaderVal) {
-    usbMIDI.sendControlChange(20, faderVal, CHANNEL);
+    usbMIDI.sendControlChange(10, faderVal, CHANNEL);
     lastFaderVal = faderVal;
     Serial.print("Fader: ");
     Serial.println(faderVal);
-    //delay(10);
   }
   if (encoderVal != oldEncoderVal) {
+    usbMIDI.sendControlChange(20, encoderVal, CHANNEL);
     oldEncoderVal = encoderVal;
     Serial.print("Encoder: ");
     Serial.println(encoderVal);
